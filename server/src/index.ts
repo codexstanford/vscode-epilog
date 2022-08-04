12a72f7e5c32ed3219babff209255aa0f4b0f15f
William O'Hanley <me@wohanley.com>
2022-08-03 19:29:50 -0700

    Warn about unsafe rules

--- server/src/index.ts
+++ server/src/index.ts
@@ -63,15 +63,72 @@ async function diagnoseDocument(textDocument: lstext.TextDocument, tree: Parser.
 
     parser.getLanguage().query(`
         (rule
-          head: (literal) @head)
-    `).captures(tree.rootNode).forEach(cap => {
-        diagnostics.push({
-            message: "Rule head",
-            range: {
-                start: ast.toLspPosition(cap.node.startPosition),
-                end: ast.toLspPosition(cap.node.endPosition)
-            },
-            severity: DiagnosticSeverity.Information
+          head: (literal) @head
+          body: (rule_body) @body)
+    `).matches(tree.rootNode).forEach(match => {
+        let head: Parser.SyntaxNode, body: Parser.SyntaxNode;
+        match.captures.forEach(capture => {
+            if (capture.name === 'head') head = capture.node;
+            if (capture.name === 'body') body = capture.node;
+        });
+
+        let checkVariables: Map<string, Parser.SyntaxNode[]> = new Map();
+
+        // Collect variables from head
+        parser.getLanguage().query(`
+            (variable) @variable
+        `).captures(head!).forEach(capture => {
+            if (!checkVariables.has(capture.node.text)) checkVariables.set(capture.node.text, []);
+            checkVariables.get(capture.node.text)!.push(capture.node);
+        });
+
+        // Collect variables from negative literals
+        parser.getLanguage().query(`
+            (literal
+              (op_negate)) @negative_literal
+        `).captures(body!).forEach(capture => {
+            const negativeLiteral = capture.node;
+            parser.getLanguage().query(`
+                (variable) @variable
+            `).captures(negativeLiteral).forEach(capture => {
+                if (!checkVariables.has(capture.node.text)) checkVariables.set(capture.node.text, []);
+                checkVariables.get(capture.node.text)!.push(capture.node);
+            });
+        });
+
+        const checkedSafe: string[] = [];
+
+        checkVariables.forEach((nodes, varName) => {
+            parser.getLanguage().query(`
+                (
+                  (variable) @variable
+                  (#match? @variable "${varName}")
+                )
+            `).captures(body).forEach(capture => {
+                // If we have an instance of the variable that isn't itself one
+                // of the potentially unsafe instances, then the variable is safe.
+                if (!checkVariables.get(varName)!.find(v => v.id === capture.node.id)) {
+                    checkedSafe.push(varName);
+                }
+            });
+        });
+
+        checkedSafe.forEach(varName => {
+            checkVariables.delete(varName);
+        });
+
+        checkVariables.forEach((nodes, varName) => {
+            nodes.forEach(node => {
+                diagnostics.push({
+                    message: `Unsafe variable: ${varName} does not appear in any positive literals in this rule.`,
+                    source: 'epilog-lsp',
+                    range: {
+                        start: ast.toLspPosition(node.startPosition),
+                        end: ast.toLspPosition(node.endPosition)
+                    },
+                    severity: DiagnosticSeverity.Warning
+                });
+            });
         });
     });
 
